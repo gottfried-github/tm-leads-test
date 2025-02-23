a56d9f3e0cea540d4a6073e17d1af9d238c20629
gottfried <gottfried.dev@gmail.com>
2025-02-23 17:02:19 +0200

    wip: work on tariffs layout

---
+++ src/app/components/icons/ArrowDown.jsx
@@ -0,0 +1,15 @@
+const ArrowDown = () => {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M6 9.5L12 15.5L18 9.5"
+        stroke="white"
+        stroke-width="1.4"
+        strokeLinecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  )
+}
+
+export default ArrowDown
